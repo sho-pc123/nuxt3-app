bfa3e0bfe430b11c8bf67668cd4ac5c2de5872d8
sho-pc123 <syouteni123@gmail.com>
2023-12-07 09:00:08 +0900

    一旦、API疎通

---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="article in data" :key="article.id">
+        {{ article.title }}
+      </li>
+    </ul>
+    <hr>
+    <NuxtLink to="/articles">Articles Page</NuxtLink>
+  </div>
+</template>
+
+<script setup>
+const { data } = await useFetch('https://qiita.com/api/v2/items?page=1&per_page=2', {
+  method: "GET",
+  headers: {
+    'Authorization': 'Bearer 2950aff10aaded5955af11c26017529143baff77'
+  }
+})
+</script>
