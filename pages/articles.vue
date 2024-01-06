6117990b42745ebb7cc8eb1cc64466e879aec29d
sho-pc123 <syouteni123@gmail.com>
2024-01-06 17:13:41 +0900

    細かい修正2

--- pages/articles.vue
+++ pages/articles.vue
@@ -11,7 +11,7 @@
 </template>
 
 <script setup>
-import pageApiStore from '@/store/page/articles/apiStore'
+import { pageApiStore } from '@/store/page/articles/apiStore'
 
 const apiStore = pageApiStore()
 apiStore.actionTitle()
