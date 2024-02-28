d05ee7e3eaf7167f33d88ad64b1e10fc88811905
sho-pc123 <syouteni123@gmail.com>
2024-02-28 09:31:37 +0900

    TSDocの追加

--- components/page-contents/articles/Qiita/main.vue
+++ components/page-contents/articles/Qiita/main.vue
@@ -13,9 +13,13 @@
 <script setup>
 import { pageApiStore } from '@/store/page/qiita-articles/apiStore'
 
+/** Page API Store */
 const apiStore = pageApiStore()
 apiStore.actionTitle()
+
+/** Qiita 記事一覧データ */
 const data = apiStore.articleTitle
+console.log(data)
 </script>
 
 <style lang="scss" scoped>
--- infrastractures/rest/qiita.com/api/v2/items/api.ts
+++ infrastractures/rest/qiita.com/api/v2/items/api.ts
@@ -1,3 +1,7 @@
+/**
+ * Qiita記事一覧取得API
+ * @returns Qiita記事一覧取得結果
+ */
 export const get = () => {
   return useFetch('https://qiita.com/api/v2/items?page=1&per_page=10', {
     method: "GET",
