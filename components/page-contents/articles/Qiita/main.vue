3da7532161bf43a401838a209166ab5a31d28182
sho-pc123 <syouteni123@gmail.com>
2024-02-28 09:32:25 +0900

    @nuxt/imageモジュールの追加

--- components/page-contents/articles/Qiita/main.vue
+++ components/page-contents/articles/Qiita/main.vue
@@ -2,6 +2,9 @@
   <div class="qiita-articles-container">
     <template v-for="article in data" :key="article.id">
       <article class="qiita-article qiita-article__container">
+        <p>投稿日{{ article.created_at }}</p>
+        <p>投稿者{{ article.user.id }}</p>
+        <NuxtImg :src="article.user.profile_image_url" ></NuxtImg>
         <p class="qiita-article__Title">{{ article.title }}</p>
         <a :href="article.url">a</a>
       </article>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+    modules: [
+    '@nuxt/image',
+  ],
   devtools: { enabled: true },
   ssr: true
 })
