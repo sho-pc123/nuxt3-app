98c4d185df604309e165a3b2a1f107133493fee3
sho-pc123 <syouteni123@gmail.com>
2024-03-16 13:54:52 +0900

    api疎通用の型作成

---
+++ infrastractures/rest/qiita.com/api/v2/items/types.ts
@@ -0,0 +1,58 @@
+interface Group {
+  createdAt: string
+  description: string
+  name: string
+  private: boolean
+  updatedAt: string
+  urlName: string
+}
+
+interface Tag {
+  name: string
+  version: [string]
+}[]
+
+interface User {
+  description: string
+  facebookId: string
+  followeesCount: number
+  followerCount: number
+  githubLoginName: string
+  id: string
+  itemsCount: number
+  linkedinId: string
+  location: string
+  name: string
+  organization: string
+  permanentId: number
+  profileImageUrl: string
+  teamOnly: string
+  twitterScreenName: string
+  websiteUrl: string
+}
+
+interface TeamMemberShip {
+  name: string
+}
+
+export interface QiitaArticleResponse {
+  rendered_body: string
+  body: string
+  coediting: boolean
+  commentsCount: number
+  createdAt: string
+  group: Group
+  id: string
+  likeCount: number
+  privata: boolean
+  reactionCount: number
+  tags: Tag
+  title: string
+  updatedAt: string
+  url: string
+  user: User
+  pageViewsCount: number
+  teamMenmbership: TeamMemberShip
+  organizationUrlName: string
+  slide: boolean
+}
