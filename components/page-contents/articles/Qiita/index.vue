d40efc37f17c9da67b93d11f500942a99ac7895a
sho-pc123 <syouteni123@gmail.com>
2024-04-21 16:47:59 +0900

    インデント調整

--- components/page-contents/articles/Qiita/index.vue
+++ components/page-contents/articles/Qiita/index.vue
@@ -3,11 +3,11 @@
       <article class="qiita-articles__itemList">
         <template v-for="article in data" :key="article.id">
           <div class="qiita-articles__item">
-          <p>投稿日{{ article.created_at }}</p>
-          <p>投稿者{{ article.user.id }}</p>
-          <NuxtImg :src="article.user.profile_image_url" ></NuxtImg>
-          <p class="qiita-article__Title">{{ article.title }}</p>
-          <a :href="article.url">a</a>
+            <p>投稿日{{ article.created_at }}</p>
+            <p>投稿者{{ article.user.id }}</p>
+            <NuxtImg :src="article.user.profile_image_url" ></NuxtImg>
+            <p class="qiita-article__Title">{{ article.title }}</p>
+            <a :href="article.url">a</a>
           </div>
         </template>
       </article>
