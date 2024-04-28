db240d031f80f4af4fd2e3a41f83cbf74d7502b0
sho-pc123 <syouteni123@gmail.com>
2024-04-28 10:53:37 +0900

    ESLintの導入

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    modules: [
-    '@nuxt/image',
-  ],
+    modules: ['@nuxt/image', "@nuxt/eslint"],
   devtools: { enabled: true },
   ssr: true
 })
